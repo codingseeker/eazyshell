ceafade049094522b7b6c240725147feb540a31f
codingseeker <likhith.s042006@gmail.com>
2026-08-30 19:57:31 +0630

    remove legacy C implementation

--- include/parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "AST.h"
-#include "lexer.h"
-
-ASTNode *parse(Token *tokens, int count);
-
-#endif
--- include/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void free_token(Token *t);
-void free_tokens(Token *tokens, int n);
-
-#endif
